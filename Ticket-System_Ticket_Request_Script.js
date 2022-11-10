ea21f093c455e2a84559f2f9ca4ab2f50e8033d8
PlocherE <112448122+PlocherE@users.noreply.github.com>
2022-11-09 18:34:33 -0800

    Update Ticket-System_Ticket_Request_Script.js
    update, changed layout and added buttons and the like

--- Ticket-System_Ticket_Request_Script.js
+++ Ticket-System_Ticket_Request_Script.js
@@ -1,16 +1,51 @@
-const userCardTemplate = document.querySelector("[data-user-template]")
-const userCardContainer = document.querySelector("[data-user-cards-container]")
+let names = [
+    "this",
+    "is",
+    "the",
+    "name",
+    "of",
+    "an",
+    "exhibit",
+]
+//sort names in ascending order
+let sortedNames = names.sort();
+console.log(sortedNames);
 
-fetch("Exhibits.txt")
-    .then(res => res.json())
-    .then(data => {
-        users = data.map(user => {
-            const card = userCardTemplate.content.cloneNode(true).children[0]
-            const header = card.querySelector("[data-header]")
-            const body = card.querySelector("[data-body]")
-            header.textContent = user.name
-            body.textContent = user.email
-            userCardContainer.append(card)
-            return { name: user.name, email: user.email, element: card }
-        })
-    })
+//reference
+let input = document.getElementById("input");
+
+//execute function on keyup
+input.addEventListener("keyup", (e) => {
+    //loop through above arrya
+    //initially remove all elements ( so if user erases a letter or adds new letter than clean revious outputs)
+    removeElements();
+    for (let i of sortedNames) {
+        //convert input to lowercase and comare with each stirng
+        if (i.toLowerCase().startsWith(input.value.toLowerCase()) && input.value != "") {
+            //create li element
+            let listItem = document.createElement("li");
+            //one common class name
+            listItem.classList.add("list-items");
+            listItem.style.cursor = "pointer";
+            listItem.setAttribute("onclick", "displayNames('" + i + "')");
+            //display matched part in bold
+            let word = "<b>" + i.substr(0, input.value.length) + "<b>";
+            word += i.substr(input.value.length);
+            //display the value in array
+            listItem.innerHTML = word;
+            document.querySelector(".list").appendChild(listItem);
+        }
+    }
+});
+
+function displayNames(value) {
+    input.value = value;
+    removeElements();
+}
+function removeElements() {
+    //clear all the item
+    let items = document.querySelectorAll(".list-items");
+    items.forEach((item) => {
+        item.remove();
+    });
+}

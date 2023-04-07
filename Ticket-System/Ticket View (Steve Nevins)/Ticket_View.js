7917215444665b84963f6e5ffae42a3c3bb566ea
Steve Nevins <80733828+babyhuey616@users.noreply.github.com>
2023-04-06 17:55:26 -0700

    Status Update To Database
    Changes in the ticket view control panel now update the actual tickets in the database.

--- Ticket-System/Ticket View (Steve Nevins)/Ticket_View.js
+++ Ticket-System/Ticket View (Steve Nevins)/Ticket_View.js
@@ -23,6 +23,26 @@ var url = window.location.href;
 var parameters = url.substring(url.indexOf('?'));
 var ticket_number = parameters.charAt(4);
 
+function dashboard_update(firstName, lastName)
+{
+        // define the callAPI function that takes a first name and last name as parameters
+            var myHeaders = new Headers();
+            // add content type header to object
+            myHeaders.append("Content-Type", "application/json");
+            // using built in JSON utility package turn object to string and store in a variable
+            var raw = JSON.stringify({"firstName":firstName,"lastName":lastName});
+            // create a JSON object with parameters for API call and store in a variable
+            var requestOptions = {
+                method: 'POST',
+                headers: myHeaders,
+                body: raw,
+                redirect: 'follow'
+            };
+            // make API call with parameters and use promises to get response
+            fetch("https://pdgpdv7npk.execute-api.us-west-2.amazonaws.com/dev", requestOptions)
+            .then(response => response.text())
+            .catch(error => console.log('error', error));
+}
 
 function load_thread()
 {
@@ -125,9 +145,11 @@ function reset_all()
 
 function update_status(date, input)
 {
-	var timestamp = date;
+	//Updating database:
+	console.log("Passing ticket " + ticket_number + " to database. Status change = " + input);
+	dashboard_update(ticket_number, input);
 	
-	input = document.getElementById("Status").value;
+	var timestamp = date;
 	
 	status_1 = input;
 	

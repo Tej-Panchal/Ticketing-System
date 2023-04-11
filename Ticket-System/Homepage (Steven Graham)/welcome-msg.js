eaf4c0ddff9f65695e4579cbcabf70379efc9886
alex-coding-3420 <alexCodingEmail475@gmail.com>
2023-04-11 15:39:22 -0700

    remove console.log

--- Ticket-System/Homepage (Steven Graham)/welcome-msg.js
+++ Ticket-System/Homepage (Steven Graham)/welcome-msg.js
@@ -1,8 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
     const username = localStorage.getItem('username');
     const permission = localStorage.getItem('permissions');
-    console.log(username);
-    console.log(permission);
   
     const welcomeMessage = document.getElementById('welcome-message');
     if (welcomeMessage) {

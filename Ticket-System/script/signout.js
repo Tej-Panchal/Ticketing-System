c58f4d701204d326780ad17bfe4f8a08ab3a6567
Steve Nevins <80733828+babyhuey616@users.noreply.github.com>
2023-04-14 11:15:59 -0700

    Added user validation.
    Must be logged in as "customer" or "technician" to access page.

--- Ticket-System/script/signout.js
+++ Ticket-System/script/signout.js
@@ -1,3 +1,10 @@
+//Validating user:
+if((String(localStorage.permissions) !== '"Technician"') && (String(localStorage.permissions) !== '"Customer"'))
+	{
+	 alert("Access Denied!");
+	 window.location.href = "error.html";
+	}
+
 function signOut(){
     localStorage.clear();
   }

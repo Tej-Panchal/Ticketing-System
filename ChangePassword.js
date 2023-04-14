51d201c426042b2449cf26443be99df9d758ff70
Steve Nevins <80733828+babyhuey616@users.noreply.github.com>
2023-04-14 10:56:15 -0700

    Added user validation.
    User must now be logged in as a technician or customer to access this page.

--- ChangePassword.js
+++ ChangePassword.js
@@ -1,3 +1,8 @@
+if((String(localStorage.permissions) !== '"Technician"') && (String(localStorage.permissions) !== '"Customer"'))
+	{
+	 alert("Access Denied!");
+	 window.location.href = "error.html";
+	}
 
 document.getElementById("submit").addEventListener("click", function(event) {
     event.preventDefault(); // prevent form submission
